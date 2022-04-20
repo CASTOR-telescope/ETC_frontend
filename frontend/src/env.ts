cb812bb2a91a5a4ca1d05ac716580e3e634afb2b
Isaac Cheng <isaac.cheng.ca@gmail.com>
2022-04-19 23:31:11 -0400

    fix API_URL

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,2 +1,2 @@
-// export const API_URL: string = window.location.href; // e.g., <http://localhost:5000/>
-export const API_URL: string = "http://localhost:5000/"; // TODO: change this to CANFAR port
+export const API_URL: string = window.location.href; // e.g., <http://localhost:5000/>
+// export const API_URL: string = "http://localhost:5000/";
