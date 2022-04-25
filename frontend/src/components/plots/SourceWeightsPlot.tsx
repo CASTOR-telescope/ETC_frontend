edc1ad2b9a88c799e303e31ac47232c525850cf5
Isaac Cheng <isaac.cheng.ca@gmail.com>
2022-04-25 10:18:39 -0400

    add log scale to source weights heat map

--- frontend/src/components/plots/SourceWeightsPlot.tsx
+++ frontend/src/components/plots/SourceWeightsPlot.tsx
@@ -1,20 +1,49 @@
 import { themeBackgroundColor } from "../DarkModeTheme";
 import ResponsivePlot from "../ResponsivePlot";
 
+/**
+ * Returns the minimum number above zero in a 1D array.
+ */
+const getMinAboveZero = (arr: number[]): number => {
+  return Math.min(...arr.filter(Number));
+};
+const getMaxAboveZero = (arr: number[]): number => {
+  return Math.max(...arr.filter(Number));
+};
+
 type SourceWeightsPlotProps = {
   numPhotometrySubmit: number;
 };
 
 const SourceWeightsPlot: React.FC<SourceWeightsPlotProps> = ({ numPhotometrySubmit }) => {
-  // Re-render plot (more robust this way because it persists across refreshes, unlike
-  // numTelescopeSaved > 0)
   if (sessionStorage.getItem("photometryParams") !== null) {
     // Update to proper sessionStorage key after
     let photometryParams = JSON.parse(`${sessionStorage.getItem("photometryParams")}`);
     let sourceWeights = JSON.parse(photometryParams["sourceWeights"]); // need to parse twice...
+    // let zauto = true;
+    // let zmax = 0; // only relevant if zauto is false (i.e., for log color scale)
+    // let zmin = 0; // only relevant if zauto is false (i.e., for log color scale)
+    let tickprefix = "";
+    if (photometryParams["useLogSourceWeights"]) {
+      // zauto = false;
+      // zmin = Math.log10(
+      //   getMinAboveZero(sourceWeights.map((row: number[]) => getMinAboveZero(row)))
+      // );
+      // let zmaxNoLog = getMaxAboveZero(
+      //   sourceWeights.map((row: number[]) => getMaxAboveZero(row))
+      // );
+      // if (Math.abs(1 - zmaxNoLog) > 3e-3) {
+      //   // approximately 1
+      //   zmax = Math.log10(zmaxNoLog);
+      // }
+      tickprefix = "1e";
+      // Must do this after zmin/zmax
+      sourceWeights = sourceWeights.map((row: number[]) =>
+        row.map((value: number) => Math.log10(value))
+      );
+    }
     let extent = photometryParams["aperExtent"]; // [xmin, xmax, ymin, ymax]
-    let effNpix = photometryParams["effNpix"];
-    // console.log("updating SourceWeightsPlot");
+    // let effNpix = photometryParams["effNpix"];
     let plotLengths = [sourceWeights.length, sourceWeights[0].length];
     let px_scale_x = (extent[1] - extent[0]) / plotLengths[1];
     let px_scale_y = (extent[3] - extent[2]) / plotLengths[0];
@@ -25,6 +54,9 @@ const SourceWeightsPlot: React.FC<SourceWeightsPlotProps> = ({ numPhotometrySubm
         data={[
           {
             z: sourceWeights,
+            // zauto: zauto,
+            // zmin: zmin,
+            // zmax: zmax,
             type: "heatmap",
             colorscale: "Electric",
             colorbar: {
@@ -33,6 +65,9 @@ const SourceWeightsPlot: React.FC<SourceWeightsPlotProps> = ({ numPhotometrySubm
                 side: "right",
                 font: { size: 14 },
               },
+              tickprefix: tickprefix,
+              // exponentformat: "power",
+              // tickformat: "<sup>.2f</sup>",
             },
             x0: extent[0] + 0.5 * px_scale_x,
             dx: px_scale_x,
@@ -66,31 +101,6 @@ const SourceWeightsPlot: React.FC<SourceWeightsPlotProps> = ({ numPhotometrySubm
             scaleanchor: "x",
           },
           margin: { t: 40 }, // if no title
-          // pad: { t: 0, b: 0, l: 0, r: 0 },
-          // margin: { t: 20, b: 20, l: 20, r: 20 },
-
-          // layout: {
-          //   autorange: false,
-          // },
-          // yaxis2: {
-          //   title: "Source Flux Density (erg s⁻¹ cm⁻² Å⁻¹)",
-          //   showgrid: false,
-          //   gridcolor: "grey",
-          //   overlaying: "y",
-          //   side: "right",
-          //   visible: true,
-          //   exponentformat: "power",
-          //   automargin: true,
-          //   rangemode: "tozero",
-          // },
-          // margin: { t: 60 },
-          // showlegend: true,
-          // // Put legend on top left
-          // legend: {
-          //   x: 0,
-          //   xanchor: "left",
-          //   y: 1,
-          // },
         }}
         useResizeHandler={true}
         config={{
@@ -109,22 +119,7 @@ const SourceWeightsPlot: React.FC<SourceWeightsPlotProps> = ({ numPhotometrySubm
       <ResponsivePlot
         // React re-renders the plot when any state, prop, or parent component changes
         divId={`source-weights-plot-${numPhotometrySubmit}`}
-        data={
-          [
-            // {
-            //   x: [],
-            //   y: [],
-            //   marker: { color: "transparent", size: 0 },
-            //   yaxis: "y",
-            // },
-            // {
-            //   x: [],
-            //   y: [],
-            //   marker: { color: "transparent", size: 0 },
-            //   yaxis: "y2",
-            // },
-          ]
-        }
+        data={[]}
         layout={{
           title: "(Relative Source Flux in Aperture)",
           font: { color: "white", size: 14 },
@@ -146,35 +141,6 @@ const SourceWeightsPlot: React.FC<SourceWeightsPlotProps> = ({ numPhotometrySubm
             range: [-1, 1],
           },
           margin: { r: 30 },
-          // xaxis: {
-          //   showgrid: true,
-          //   gridcolor: "grey",
-          //   title: "Wavelength (Å)",
-          //   range: [1000, 6000],
-          //   visible: true,
-          // },
-          // yaxis: {
-          //   showgrid: true,
-          //   gridcolor: "grey",
-          //   title: "Passband Reponse",
-          //   range: [0, 1],
-          //   visible: true,
-          //   rangemode: "tozero",
-          // },
-          // yaxis2: {
-          //   title: "Source Flux Density (erg s⁻¹ cm⁻² Å⁻¹)",
-          //   showgrid: false,
-          //   gridcolor: "grey",
-          //   overlaying: "y",
-          //   side: "right",
-          //   // range: [0, 1e-15],
-          //   visible: true,
-          //   exponentformat: "power",
-          //   automargin: true,
-          //   rangemode: "tozero",
-          // },
-          // margin: { t: 26 },
-          // showlegend: false,
           annotations: [
             {
               text: "Please submit a Photometry<br />calculation first",
